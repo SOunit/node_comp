c4cdaa394069b8ca8aef0d01634f2f25f82ca9ac
SOunit <workbookwork7@gmail.com>
2020-11-15 20:49:21 +0900

    added 404 page controller

--- node/app.js
+++ node/app.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error.js');
+
 const app = express();
 
 // setup ejs view
@@ -17,9 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
