d43647bd2a19f6757872bebd758bbec02f603d3f
SOunit <workbookwork7@gmail.com>
2020-12-16 09:08:15 +0900

    added initializing of edit page

--- node/controllers/admin.js
+++ node/controllers/admin.js
@@ -2,9 +2,10 @@ const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
+    editing: false,
     formsCSS: true,
     productCSS: true,
     activeAddProduct: true,
@@ -18,6 +19,28 @@ exports.getAdminProducts = (req, res, next) => {
       pageTitle: 'Products',
       prods: products,
       path: '/admin/products',
+      formsCSS: true,
+      productCSS: true,
+      activeAddProduct: true,
+    });
+  });
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      prod: product,
       formsCSS: true,
       productCSS: true,
       activeAddProduct: true,
